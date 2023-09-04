2977dab5fbb62e341d58d067b84ce083628283cc
micha <micha@DESKTOP-OJC8B9B>
2023-09-04 18:20:27 -0300

    Adicionar arquivos de projeto.

---
+++ Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CursoCSharp.Fundamentos {
+    public class PrimeiroPrograma {
+        public static void Executar() {
+            System.Console.Write("Primeiro");
+            System.Console.WriteLine("Programa");
+            Console.WriteLine("Terminou!!!");
+        }
+    }
+}
